864a3faf34f2199d2f4c59d81a1ca478d948d607
ttemel <Ttev@tutev111>
2015-11-22 12:36:32 +0200

    Düzenleme

--- TutevERP/src/main/java/org/tutev/web/erp/controller/genel/KisiController.java
+++ TutevERP/src/main/java/org/tutev/web/erp/controller/genel/KisiController.java
@@ -1,33 +1,57 @@
 package org.tutev.web.erp.controller.genel;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.tutev.web.erp.entity.genel.Kisi;
+import org.tutev.web.erp.service.KisiService;
 
 @Controller("kisiController")
 @Scope("session")
-public class KisiController implements Serializable{
+public class KisiController implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3607896108305768125L;
 
-	private String isimSoyisim;
-	
-	
+	@Autowired
+	private transient KisiService kisiService;
+
+	private Kisi kisi;
+	List<Kisi> kisiListesi;
+
 	@PostConstruct
 	private void init() {
-		setIsimSoyisim("Taner ABC");  
+		kisi = new Kisi();
+		kisiListesi=kisiService.getAll();
 	}
+
+	public void kisiKaydet() {
 		
-	public String getIsimSoyisim() {
-		return isimSoyisim;
-	}	
-	public void setIsimSoyisim(String isimSoyisim) {
-		this.isimSoyisim = isimSoyisim;
+		kisi.setDogumTarihi(new Date());
+		kisiService.save(kisi);
+	}
+
+	public Kisi getKisi() {
+		if (kisi == null) {
+			kisi = new Kisi();
+		}
+		return kisi;
+	}
+
+	public void setKisi(Kisi kisi) {
+		this.kisi = kisi;
+	}
+	
+	public List<Kisi> getKisiListesi() {
+		return kisiListesi;
 	}
 }
