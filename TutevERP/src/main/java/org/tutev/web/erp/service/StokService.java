cb6a9a4e39900e5f251f9ff7a9d58724f35e215f
celikn <naimecelik@gmail.com>
2015-11-22 12:47:39 +0200

    Stok Son Gonderme

--- TutevERP/src/main/java/org/tutev/web/erp/service/StokService.java
+++ TutevERP/src/main/java/org/tutev/web/erp/service/StokService.java
@@ -4,62 +4,56 @@ import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.tutev.web.erp.entity.stok.StokKart;
+
+@Service("stokService")
 public class StokService implements ServiceBase<StokKart>{
-			
+	 
+	
+	
+	 @Autowired
+	 private transient BaseDao baseDao;
+	  
 	 @Override
 	    public StokKart save(StokKart entity) {
-	        Session session = getSession();
-	        Transaction t= session.getTransaction();
-	        t.begin();
-	        session.save(entity);
-	        t.commit();
+		 	baseDao.save(entity);
 	        return entity;
-	    }
+	 }
 	 
 	 @Override
 	    public StokKart update(StokKart entity) {
-	        Session session = getSession();
-	        Transaction t = session.getTransaction();
-	        t.begin();
-	        session.saveOrUpdate(entity);
-	        t.commit();
+		    baseDao.saveOrUpdate(entity);
 	        return entity;
-	    }
+	 }       
 	 
 	 @Override
 	    public Boolean delete(StokKart entity) {
-	        try {
-	            Session session = getSession();
-	            Transaction t= session.getTransaction();
-	            t.begin();
-	            session.delete(entity);
-	            t.commit();
-	            session.getTransaction().commit();
-	        } catch (Exception e) {
+		 try {
+	        	baseDao.delete(entity);
+	        } catch (Exception ex) {
 	            return false;
 	        }
-
 	        return true;
 	    }
 
+	 
 	    @Override
 	    public StokKart getById(Long id) {
 	        Session session = getSession();
-	        StokKart kart = (StokKart) session.get(StokKart.class, id);
-	        return kart;
+	        return (StokKart) session.get(StokKart.class, id);
 	    }
 
 	    @Override
 	    public List<StokKart> getAll() {
 	        Criteria criteria=getSession().createCriteria(StokKart.class);
-	        return (List<StokKart>) criteria.list();
+	        return criteria.list();
 	    }
 
 	    @Override
 	    public Session getSession() {
-	    	return null;
+	    	return baseDao.getSession();
 	    }
 	 
 	 
